d4f4194661dd6e8ccd897deaacc05503301854e2
Hanntuu <raihanrrdarmawan@gmail.com>
2023-08-11 11:21:04 +0700

    add login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom'
 import Home from "./Pages/Home"
 import Features from "./Pages/Features"
 import Login from "./Pages/Login"
+import Dashboard from "./Pages/Dashboard"
 
 function App() {
   return (
@@ -11,9 +12,10 @@ function App() {
         <Route path="/" Component={Home}/>
         <Route path="/Features" Component={Features}/>
         <Route path="/Login" Component={Login}/>
+        <Route path="/Dashboard" Component={Dashboard}/>
       </Routes>
     </div>
   )
 }
 
-export default App
+export default App
--- src/Components/FormLogin.jsx
+++ src/Components/FormLogin.jsx
@@ -1,26 +1,31 @@
 import React from "react";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
 const FormLogin = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
-    <div>
-      <div onClick ={() => navigate ("/")} className="flex flex-wrap justify-center py-36 hover:cursor-pointer">
+    <div className="flex flex-col items-center h-screen">
+      <div
+        onClick={() => navigate("/")}
+        className="flex flex-wrap justify-center py-28 hover:cursor-pointer"
+      >
         <img src="/Logo.png" alt="Logo" className="w-26 h-16" />
         <a className=" font-jakarta font-extrabold text-[25px] my-3 text-primary tracking-[.15em]">
           PROBOX
         </a>
       </div>
-      <div className="flex flex-col items-center h-screen">
-        <div className="card w-96 bg-base-100 shadow-xl ">
-          <div className="card-body text-center">
-            <h2 className="card-title">Sign In</h2>
-            <p>If a dog chews shoes whose shoes does he ?</p>
-            <div className="card-actions justify-end"></div>
+      <div className="card w-96 bg-base-100  shadow-xl my-3">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title font-jakarta font-bold text-[30px] ">Sign in to your</h2>
+          <h2 className="card-title font-jakarta font-extrabold text-[30px] text-primary">account </h2>
+          <p className="font-jakarta my-3 text-[15px]">Put it, Leave it  </p>
+          <div className="card-actions justify-end my-3">
+            <button className="btn bg-base-200">
+            <img src="/google.png" alt="" className="w-5 h-5"/>Sign in with google</button>
           </div>
         </div>
+      </div>  
       </div>
-    </div>
   );
 };
-export default FormLogin;
+export default FormLogin;
