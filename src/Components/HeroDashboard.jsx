78de75cfcb48a45835a21153b3acb13fc1164f01
Hanntuu <raihanrrdarmawan@gmail.com>
2023-08-14 14:12:59 +0700

    add auth login

--- src/Components/HeroDashboard.jsx
+++ src/Components/HeroDashboard.jsx
@@ -1,25 +1,59 @@
+
 import React from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+const BoxItem = ({ boxId, uid, timestamps }) => (
+  <div className="w-full p-4">
+    <div className="card bg-primary text-primary-content">
+      <div className="card-body">
+        <p className="font-bold text-lg">BOX ID: {boxId}</p>
+        <p>UID: {uid}</p>
+        {timestamps.map((timestamp, index) => (
+          <p key={index}>{timestamp}</p>
+        ))}
+      </div>
+    </div>
+  </div>
+);
 
 const HeroDashboard = () => {
+  const boxes = [
+    {
+      boxId: 2,
+      uid: "002",
+      timestamps: ["11/08/2023 - 14.24", "11/08/2023 - 16.00"],
+    },
+    {
+      boxId: 3,
+      uid: "003",
+      timestamps: ["11/08/2023 - 14.24", "11/08/2023 - 16.00"],
+    },
+    {
+      boxId: 4,
+      uid: "004",
+      timestamps: ["11/08/2023 - 14.24", "11/08/2023 - 16.00"],
+    },
+    {
+      boxId: 5,
+      uid: "005",
+      timestamps: ["11/08/2023 - 14.24", "11/08/2023 - 16.00"],
+    },
+  ];
   const navigate = useNavigate();
   return (
     <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-      <div>
-        <div onClick={() => navigate("/")}
-        className="flex flex-wrap justify-center py-5 hover:cursor-pointer">
-        <img src="/Logo.png" alt="Logo" className="w-26 h-16" />
-        <a className=" font-jakarta font-extrabold text-[25px] my-3 text-primary tracking-[.15em]">
-          PROBOX
-        </a>
+      <div className="flex flex-col sm:flex-row justify-between items-center bg-base-100 p-4">
+        <div onClick={() => navigate("/") }className="flex items-center space-x-2 sm:justify-center lg:justify-start hover:cursor-pointer">
+          <img src="/Logo.png" alt="Logo" className="w-26 h-16" />
+          <a className="font-jakarta font-extrabold text-[25px] my-3 text-primary tracking-[.15em]">
+            PROBOX
+          </a>
         </div>
-        <div className="hidden sm:flex">
-        {/* <button
-          onClick={() => navigate("/Login")}
-          className="btn btn-primary text-white"
-        >
-          SIGN IN
-        </button> */}
+        <div className="mt-4 sm:mt-0 sm:ml-auto">
+          <button className="btn btn-primary sm:self-center lg:self-start">
+            SIGN OUT
+          </button>
+        </div>
       </div>
       <div className="flex flex-col mt-6 sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
         <div className="w-full flex justify-center">
@@ -44,48 +78,15 @@ const HeroDashboard = () => {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
-        <div className="w-full p-4">
-          <div className="card bg-primary text-primary-content">
-            <div className="card-body">
-              <p className="font-bold text-lg">BOX ID: 2</p>
-              <p>UID: 002</p>
-              <p>11/08/2023 - 14.24</p>
-              <p>11/08/2023 - 16.00</p>
-            </div>
-          </div>
-        </div>
-        <div className="w-full p-4">
-          <div className="card bg-primary text-primary-content">
-            <div className="card-body">
-              <p className="font-bold text-lg">BOX ID: 3</p>
-              <p>UID: 003</p>
-              <p>11/08/2023 - 14.24</p>
-              <p>11/08/2023 - 16.00</p>
-            </div>
-          </div>
-        </div>
-        <div className="w-full p-4">
-          <div className="card bg-primary text-primary-content">
-            <div className="card-body">
-              <p className="font-bold text-lg">BOX ID: 4</p>
-              <p>UID: 004</p>
-              <p>11/08/2023 - 14.24</p>
-              <p>11/08/2023 - 16.00</p>
-            </div>
-          </div>
-        </div>
-        <div className="w-full p-4">
-          <div className="card bg-primary text-primary-content">
-            <div className="card-body">
-              <p className="font-bold text-lg">BOX ID: 5</p>
-              <p>UID: 005</p>
-              <p>11/08/2023 - 14.24</p>
-              <p>11/08/2023 - 16.00</p>
-            </div>
-          </div>
-        </div>
+        {boxes.map((box) => (
+          <BoxItem
+            key={box.boxId}
+            boxId={box.boxId}
+            uid={box.uid}
+            timestamps={box.timestamps}
+          />
+        ))}
       </div>
-    </div>
     </div>
   );
 };
---
+++ src/Components/firebaseConfig.js
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtl_9_flWOaPoxKwNX8ZjsBCP6KLkhaDA",
+  authDomain: "pro-box-fac33.firebaseapp.com",
+  projectId: "pro-box-fac33",
+  storageBucket: "pro-box-fac33.appspot.com",
+  messagingSenderId: "585652928316",
+  appId: "1:585652928316:web:8c5093456fc853a5d6e839",
+  measurementId: "G-11F0H13WVH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export {auth, provider}
