88f5eba5de9af098c1233918e7d39a9f5a64d2c8
froxyroar <rifkirosada7@gmail.com>
2023-08-16 14:58:17 +0700

    adding 404 page pt2.

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom"; // Assuming you're using React Router
 const NotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-blue-600 to-red-800">
-      <img src="/Logo.png" alt="Logo" className="w-40 h-40 mb-4 grayscale brightness-150" />
-      <h1 className="text-4xl font-semibold mb-2 text-white">404</h1>
+      <img src="/Logo.png" alt="Logo" className="w-40 h-40 animate-bounce mb-4 grayscale brightness-150" />
+      <h1 className="text-5xl font-semibold mb-2 text-white">404</h1>
       <p className="text-lg text-white text-center mb-6">
-        Oops! Looks like you've wandered into uncharted territory.
+        Oops! It seems you've taken a wrong turn. This page doesn't exist.
       </p>
       <Link
         to="/"
